d2f32a3a086845eb124972a12b8bdee55d2371e0
Chandrashekhar Singh Mourya <118621709+Singh-csm@users.noreply.github.com>
2023-12-28 23:34:26 +0530

    Update action.js

--- src/Redux/Tour Detail/action.js
+++ src/Redux/Tour Detail/action.js
@@ -6,7 +6,7 @@ export const GET_SINGLE_TOUR_REQUEST = "GET_SINGLE_TOUR_REQUEST";
 export const singleTour = (id) => async (dispatch) => {
     try {
         console.log(id)
-        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/travelious_tour/${id}`)
+        const response = await axios.get(`https://longhaired-talented-broccoli.glitch.me/travelious_tour/${id}`)
         console.log(response)
         dispatch({type:GET_SINGLE_TOUR_REQUEST,payload:response.data})
     } catch (error) {
